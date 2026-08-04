d9fcbb78c40e548ad39c7ca7fddda5bcdca21af0
Hardik Patel <hardikpatelse@users.noreply.github.com>
2026-08-04 18:06:03 +0530

    style: run nx format

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -4,24 +4,24 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'static',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: { github: 'https://github.com/withastro/starlight' },
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  output: 'static',
+  integrations: [
+    starlight({
+      title: 'My Docs',
+      social: { github: 'https://github.com/withastro/starlight' },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
