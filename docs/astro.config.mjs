ea0c3688e5050808b6b1e6326bf6ad9a37321e8f
Hardik Patel <hardikpatelse@users.noreply.github.com>
2026-08-07 23:24:59 +0530

    chore: update package-lock.json dependencies

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     starlight({
       title: 'My Docs',
       social: {
-        github: 'https://github.com/withastro/starlight'
+        github: 'https://github.com/withastro/starlight',
       },
       sidebar: [
         {
